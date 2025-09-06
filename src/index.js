7570c0c6a424903ca28deba00ad3f1ef5fa26c9e
ppopp001234 <ppopp001234@gmail.com>
2025-09-07 05:23:18 +0800

    w

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // 如果你有 CSS
 
-const App = () => <h1>🎰 Casino App Ready!</h1>;
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
